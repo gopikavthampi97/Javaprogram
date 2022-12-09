3fb88276566ff5d535d8d5578283bf4b65fc7765
gopikavthampi97 <gopikavasantham77@gmail.com>
2022-12-09 11:29:29 +0530

    intial commit

---
+++ additionof2array.java
@@ -0,0 +1,14 @@
+class additionof2array
+{
+public static void main(String args[])
+{
+int a[] ={1,2,3,4,5};
+int b[] ={1,2,6,4,8};
+for (int i=0;i<5;i++)
+{
+ int c=a[i]+b[i];
+ System.out.println (c);
+}
+
+}
+}
